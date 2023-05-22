0fd1d3b0c8d84b8c159cd2a22c68a7852d604c24
Fernando Hillebrand <fer.hill@gmail.com>
2023-05-22 15:02:51 -0300

    fix some produccion issue and remove the container-name in all the docker-compose.yml

--- front/next.config.js
+++ front/next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-        ],
-    },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  excludeFile: (str) =>
+    /\*.{spec,test}.js/.test(str) || str.includes('stories.tsx'),
 };
 
 module.exports = nextConfig;
